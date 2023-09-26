3c39692f03b05a0b7bbc639e0afc77feceba843e
MyNameIsJohnson <johnsonle2929@gmail.com>
2023-09-26 08:53:38 -0500

    Removed unused fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,9 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import PostCreate from "./PostCreate";
 import PostList from "./PostList";
 
 function App() {
-  const [content, setContent] = useState("");
-  const getPosts = (req, res) => {
-    fetch("http://localhost:4000/posts")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("get posts", data);
-        setContent(data);
-        //res.send({message:'success'});
-      });
-  };
   return (
     <div className="container">
       <h1>Create Post</h1>
